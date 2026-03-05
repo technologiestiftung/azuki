d50464ab756741d7f720a7c8be954a4288658d80
Zainab Tariq <zainabtariq12@gmail.com>
2026-03-05 16:04:26 +0100

    feat: update start sceen

--- frontend/src/components/competence-profile/start/StartScreen.tsx
+++ frontend/src/components/competence-profile/start/StartScreen.tsx
@@ -9,23 +9,25 @@ export function StartScreen() {
 	const nextStep = useAppStore((state) => state.nextStep);
 
 	return (
-		<div className="flex flex-col min-h-[100dvh] p-4">
-			<div className="pb-1">
+		<div className="flex flex-col h-[100dvh] pt-4 overflow-y-auto min-h-0">
+			<div className="pb-1 px-4">
 				<BackButton onClick={() => goToStep(Step.Welcome)} />
 			</div>
 
-			<div className="flex-1 flex flex-col items-center justify-center">
+			<div className="flex-1 flex flex-col items-center justify-center px-4">
 				<img src="/illustrations/star.svg" alt="" className="w-full" />
 			</div>
 
-			<div className="flex flex-col gap-4 py-6">
+			<div className="flex flex-col gap-4 py-6 mb-4 px-4">
 				<h1 className="text-4xl font-bold">{content["start.title"]}</h1>
 				<p className="text-lg font-normal">{content["start.description"]}</p>
 			</div>
 
-			<PrimaryButton onClick={nextStep} className="w-full mt-8">
-				{content["start.cta"]}
-			</PrimaryButton>
+			<div className="w-full bg-white border-t-2 border-gray-200 flex flex-col p-4 gap-y-2 max-w-[430px]">
+				<PrimaryButton onClick={nextStep} className="w-full">
+					{content["start.cta"]}
+				</PrimaryButton>
+			</div>
 		</div>
 	);
 }
