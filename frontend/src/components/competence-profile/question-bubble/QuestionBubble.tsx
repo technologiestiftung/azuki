0d31da0aa4d0598c371d79e2aa40af5da0b4a3d2
Zainab Tariq <zainabtariq12@gmail.com>
2026-03-12 18:13:15 +0100

    fix: improve accessibility in QuestionBubble component

--- frontend/src/components/competence-profile/question-bubble/QuestionBubble.tsx
+++ frontend/src/components/competence-profile/question-bubble/QuestionBubble.tsx
@@ -9,23 +9,24 @@ export function QuestionBubble({ children }: QuestionBubbleProps) {
 		<div className="w-full pt-[38px] bg-gradient-to-b from-sky-white from-50% to-transparent">
 			<div className="relative w-full">
 				<div className="relative w-full">
-					<div className="absolute -top-[35px] left-1.5">
+					<div className="absolute -top-[35px] left-1.5" aria-hidden="true">
 						<div className="relative">
 							<img
 								src="/illustrations/question-bubble-star.svg"
-								alt="Question Bubble"
+								alt=""
 								className="relative"
 							/>
 							<img
 								src="/illustrations/eyes.svg"
-								alt="Question Bubble"
+								alt=""
 								className="absolute top-[22px] left-6 z-10"
 							/>
 						</div>
 					</div>
 					<img
 						src="/illustrations/question-bubble-outline.svg"
-						alt="Question Bubble"
+						alt=""
+						aria-hidden="true"
 						className="absolute inset-0 h-full w-full"
 					/>
 					<div className="relative flex items-center p-3">{children}</div>
