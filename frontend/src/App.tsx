2da8816abda72781e68b1d4a5a61568b0944971e
nlspnsgen <niels.poensgen@ts.berlin>
2026-04-07 16:48:43 +0200

    feat: add dev-mode floating bar with model selector and cost display

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,6 @@
 import { Routes, Route, Navigate, useLocation } from "react-router-dom";
+import { DevBar } from "./components/dev-bar/DevBar";
+import { useDevMode } from "./hooks/useDevMode";
 import { LoginScreen } from "./components/login-screen/LoginScreen";
 import { WelcomeCarousel } from "./components/welcome-screen/WelcomeCarousel";
 import { StartScreen } from "./components/competence-profile/start/StartScreen";
@@ -18,6 +20,7 @@ import { ROUTE_PATHS } from "./routing/routes";
 
 function App() {
 	const location = useLocation();
+	const devMode = useDevMode();
 
 	return (
 		<div className="max-w-[430px] mx-auto h-[100dvh] bg-sky-white relative overflow-hidden">
@@ -62,6 +65,7 @@ function App() {
 					/>
 				</Routes>
 			</div>
+		{devMode && <DevBar />}
 		</div>
 	);
 }
