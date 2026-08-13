38c49bf87a7a8854bfcfc2659bf7870454910117
Zainab Tariq <zainabtariq12@gmail.com>
2026-08-13 10:21:13 +0200

    feat: add entrance  animations for inSchool step (#112)

--- frontend/src/components/competence-profile/steps/StepLayout.tsx
+++ frontend/src/components/competence-profile/steps/StepLayout.tsx
@@ -4,6 +4,7 @@ import { ProgressBar } from "../progress-bar/ProgressBar";
 import { QuestionBubble } from "../question-bubble/QuestionBubble";
 import { content } from "../../../content";
 import { PrimaryButton } from "../../primitives/buttons/PrimaryButton";
+import { PrimaryThemedButton } from "../../primitives/buttons/PrimaryThemedButton";
 import { GhostButton } from "../../primitives/buttons/GhostButton";
 import { BackButton } from "../../back-button/BackButton";
 import { getGranularProgress, getPreviousPath } from "../../../routing/routes";
@@ -25,6 +26,9 @@ interface StepLayoutProps {
 	hasNextButton?: boolean;
 	skipLabel?: string;
 	bottomContent?: ReactNode;
+	animateEnter?: boolean;
+	themedNextButton?: boolean;
+	showFooterBorder?: boolean;
 }
 
 export function StepLayout({
@@ -39,10 +43,14 @@ export function StepLayout({
 	hasNextButton = true,
 	skipLabel,
 	bottomContent,
+	animateEnter = false,
+	themedNextButton = false,
+	showFooterBorder = true,
 }: StepLayoutProps) {
 	const { pathname, hash } = useLocation();
 	const navigate = useNavigate();
 	const progress = getGranularProgress(pathname, hash);
+	const NextButton = themedNextButton ? PrimaryThemedButton : PrimaryButton;
 
 	const handleBack = () => {
 		closeToast();
@@ -61,36 +69,48 @@ export function StepLayout({
 	return (
 		<div className="relative flex flex-col h-[100dvh] py-4">
 			<Toast />
-			<div className="flex items-center gap-3 pb-1 shrink-0 px-4">
-				<BackButton onClick={handleBack} />
-				<div className="flex-1">
-					<ProgressBar progress={progress} />
+			<div
+				className={`flex flex-1 flex-col min-h-0 ${
+					animateEnter ? "animate-stepEnterFromTop" : ""
+				}`}
+			>
+				<div className="flex items-center gap-3 pb-1 shrink-0 px-4">
+					<BackButton onClick={handleBack} />
+					<div className="flex-1">
+						<ProgressBar progress={progress} />
+					</div>
 				</div>
-			</div>
-			<div className="flex flex-1 flex-col min-h-0">
-				<div className="shrink-0 px-4">
-					<QuestionBubble question={question} subtitle={subtitle} />
-				</div>
+				<div className="flex flex-1 flex-col min-h-0">
+					<div className="shrink-0 px-4">
+						<QuestionBubble question={question} subtitle={subtitle} />
+					</div>
 
-				<div
-					className={`flex flex-1 flex-col min-h-0 overflow-x-clip overflow-y-auto overscroll-y-contain overscroll-x-none touch-pan-y pt-8 px-4 ${hasSkipButton ? "pb-[110px]" : "pb-20"}`}
-				>
-					{children}
+					<div
+						className={`flex flex-1 flex-col min-h-0 overflow-x-clip overflow-y-auto overscroll-y-contain overscroll-x-none touch-pan-y pt-8 px-4 ${hasSkipButton ? "pb-[110px]" : "pb-20"}`}
+					>
+						{children}
+					</div>
 				</div>
 			</div>
 
 			<div
-				className={`fixed bottom-0 left-1/2 z-30 -translate-x-1/2 w-full bg-sky-white border-t-2 border-gray-200 flex flex-col px-4 gap-y-1 max-w-[430px] ${hasSkipButton ? "pt-4 pb-2" : "py-4"}`}
+				className={`fixed bottom-0 left-0 right-0 z-30 mx-auto w-full flex flex-col px-4 gap-y-1 max-w-[430px] ${
+					showFooterBorder
+						? "bg-sky-white border-t-2 border-gray-200"
+						: "border-t-0 bg-transparent"
+				} ${hasSkipButton ? "pt-4 pb-2" : "py-4"} ${
+					animateEnter ? "animate-stepCtaEnterFromBottom" : ""
+				}`}
 			>
 				{bottomContent}
 				{hasNextButton && (
-					<PrimaryButton
+					<NextButton
 						onClick={onNext}
 						disabled={isNextDisabled}
 						className="w-full"
 					>
 						{content["navigation.next"]}
-					</PrimaryButton>
+					</NextButton>
 				)}
 
 				{hasSkipButton && (
