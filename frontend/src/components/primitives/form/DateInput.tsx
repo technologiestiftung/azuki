96ab9336961edb041e06a1f8bdd96787a9ae3905
aeschi <56318362+aeschi@users.noreply.github.com>
2026-08-20 14:13:35 +0200

    fix: svg animation and calender appearence for mobile (#122)
    Signed-off-by: aeschi <56318362+aeschi@users.noreply.github.com>

--- frontend/src/components/primitives/form/DateInput.tsx
+++ frontend/src/components/primitives/form/DateInput.tsx
@@ -18,7 +18,7 @@ export function DateInput({
 	return (
 		<input
 			type="date"
-			className={`w-full h-12 px-3 py-2 rounded-lg border-2 ${
+			className={`block w-full min-w-0 max-w-full appearance-none bg-white h-12 px-3 py-2 text-base rounded-lg border-2 ${
 				error ? "border-red-600" : "border-gray-400"
 			} focus:outline-none focus:ring-2 focus:ring-sky-300 ${calendarPickerClassName} ${className}`}
 			aria-invalid={error}
